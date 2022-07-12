cad04f6446264a004ea7435bcd0d337b4cd66fff
Vinícios Oliveira Grama <vinyirun1@gmail.com>
2022-07-12 13:55:50 -0300

    fix(test): correct test graphql query me

--- packages/server/src/graphql/schema/__tests__/QueryType.spec.ts
+++ packages/server/src/graphql/schema/__tests__/QueryType.spec.ts
@@ -5,11 +5,7 @@ import { createUser } from "../../../modules/user/fixtures/createUser";
 import { schema } from "../schema";
 
 it("should return logged user", async () => {
-  const user = await createUser({
-    name: "Test",
-    username: "Test",
-    phone: "5577999999999",
-  });
+  const user = await createUser({});
 
   // language=GraphQL
   const query = `
@@ -34,7 +30,7 @@ it("should return logged user", async () => {
   });
 
   expect(result.errors).toBeUndefined();
-  expect((result.data as any).me).not.toBe(null);
+  expect((result.data as any).me).not.toBeNull();
   expect((result.data as any).me.name).toBe(user.name);
   expect((result.data as any).me.username).toBe(user.username);
 });
